d60fd0344c2c24f13dd32e61fe46b09eaed73c40
alok <alokbarmanraj2021@gmail.com>
2026-03-31 23:17:04 +0600

    recup code

--- Components/DaisyUi/PricingCard.jsx
+++ Components/DaisyUi/PricingCard.jsx
@@ -5,16 +5,16 @@ const PricingCard = () => {
   return (
     <div className="bg-white pt-22 pb-22">
       <div className="max-w-9/12 mx-auto">
-
         <div className="text-center pb-8 space-y-3 text-black">
-          <h1 className="text-4xl lg:text-5xl font-bold">Simple, Transparent Pricing</h1>
+          <h1 className="text-4xl lg:text-5xl font-bold">
+            Simple, Transparent Pricing
+          </h1>
           <p className="text-slate-600">
             Choose the plan that fits your needs. Upgrade or downgrade anytime.
           </p>
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-
           <div className="bg-[#f8f8f8] border border-blue-100 p-5 rounded-2xl space-y-4">
             <div>
               <h3 className="text-2xl font-bold text-black">Starter</h3>
@@ -47,8 +47,12 @@ const PricingCard = () => {
             </button>
           </div>
 
-
-          <div className="bg-gradient-to-r from-[#4A00E0] via-[#6A11CB] to-[#8E2DE2] text-white p-5 rounded-2xl space-y-4">
+          <div className="relative bg-gradient-to-r from-[#4A00E0] via-[#6A11CB] to-[#8E2DE2] text-white p-5 rounded-2xl space-y-4">
+            <div className="absolute -top-4 left-1/2 transform -translate-x-1/2">
+              <div className="w-30 bg-[#FEF3C6] p-1 rounded-full text-center">
+                <p className="text-[#BB4D00]">Most Popular</p>
+              </div>
+            </div>
             <div>
               <h3 className="text-2xl font-bold">Pro</h3>
               <p>Best for professionals</p>
@@ -84,10 +88,11 @@ const PricingCard = () => {
               </div>
             </div>
             <button className="btn bg-white w-full rounded-full border-none">
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4A00E0] via-[#6A11CB] to-[#8E2DE2] px-5">Start Pro Trial</span>
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4A00E0] via-[#6A11CB] to-[#8E2DE2] px-5">
+                Start Pro Trial
+              </span>
             </button>
           </div>
-
 
           <div className="bg-[#f8f8f8] border border-blue-100 p-5 rounded-2xl space-y-4">
             <div>
